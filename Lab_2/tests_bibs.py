3096a819314b9557377fe756fcf33e78cfc5b92f
rfbittar <rafabittarg@gmail.com>
2025-04-10 15:26:42 -0300

    feat: asdasdasd

--- Lab_2/tests_bibs.py
+++ Lab_2/tests_bibs.py
@@ -23,3 +23,32 @@ class CNode:
             raise TypeError("Esperado um objeto do tipo VNode")
         self.v_nodes.append(vnode)
         self.dc = len(self.v_nodes)
+
+def LDPC(dv, dc, N):
+    if (N * dv) % dc != 0:
+        raise ValueError("N * dv deve ser divisível por dc para um LDPC regular.")
+
+    M = (N * dv) // dc 
+    all_vnodes = [VNode() for _ in range(N)]
+    all_cnodes = [CNode() for _ in range(M)]
+
+    connections = []
+
+    for i in range(N):
+        connections += [i] * dv
+
+    import random
+    random.shuffle(connections)
+
+    idx = 0
+    for cnode in all_cnodes:
+        for _ in range(dc):
+            vnode_index = connections[idx]
+            vnode = all_vnodes[vnode_index]
+            cnode.add_v_node(vnode)
+            vnode.add_v_node(cnode)
+            idx += 1
+
+    return all_vnodes, all_cnodes
+
+LDPC(3,7,7)
