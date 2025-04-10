69984e3350da8b9217b653a8a67e0c52a3e5c22b
davil2020 <davilessa2002@gmail.com>
2025-04-10 13:59:03 -0300

    .

---
+++ test.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define _ ios_base::sync_with_stdio(false); cin.tie(0);
+#define rep(i,a,b) for(int i = a; i <= b; i++)
+#define f first
+#define s second
+#define pb push_back
+#define MAXN 200010
+ 
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+ 
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+ 
+ 
+void solve() {
+    int n;
+    cin >> n;
+}
+ 
+ 
+int main() {
+    _
+    ll t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
