0f06b6164554d34f4bca9c610bbda20da1d0700b
rfbittar <rafabittarg@gmail.com>
2025-04-10 14:43:00 -0300

    feat: v0 classes dos nos

--- Lab_2/tests_bibs.py
+++ Lab_2/tests_bibs.py
@@ -1,11 +1,25 @@
+class VNode:
+    def __init__(self, val: int = 0):
+        if val not in (0, 1):
+            raise ValueError("val deve ser 0 ou 1")
+        self.val = val
+        self.v_nodes = []
+        self.dv = 0
 
-# %% Bloco 1
-x = 10
-y = 5
-print("Soma:", x + y)
-
-# %% Bloco 2
-print("Multiplicação:", x * y)
+    def add_v_node(self, vnode):
+        if not isinstance(vnode, VNode):
+            raise TypeError("Esperado um objeto do tipo VNode")
+        self.v_nodes.append(vnode)
+        self.dv = len(self.v_nodes)
 
 
-# %%
+class CNode:
+    def __init__(self):
+        self.v_nodes = []
+        self.dc = 0
+
+    def add_v_node(self, vnode):
+        if not isinstance(vnode, VNode):
+            raise TypeError("Esperado um objeto do tipo VNode")
+        self.v_nodes.append(vnode)
+        self.dc = len(self.v_nodes)
