4707532af0414104e6b1d3208dc1c9c87ab9f3f1
davil2020 <davilessa2002@gmail.com>
2025-04-10 14:09:29 -0300

    .

---
+++ Lab_2/test.py
@@ -0,0 +1,8 @@
+# Calculadora de soma simples
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+soma = num1 + num2
+
+print("A soma é:", soma)
